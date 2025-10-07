14fe5bb1e3aa581bdf288eaeb6526839c3719b5b
masayyasy <84440144+masayyasy@users.noreply.github.com>
2025-10-07 09:10:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ let propertis = JSON.parse(localStorage.getItem('propertiData')) || [];
 let dataReferensi = JSON.parse(localStorage.getItem('dataReferensi')) || {};
 
 // URL Web App Google Apps Script
-const GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw3yk0kD-YkhiLSJ65-ipgrxLYjKPCPDcqAJ4ruawLX9ww92jn0UlnkgYl7Cd9d1LA7dw/exec"; // ganti dengan URL Web App kamu
+const GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyV1DNN84OEd4sOIVsgs2ujb6amk6kGNUQiuq_LxecglEVdqJmIG_SmOLkwqqasqzU92g/exec"; // ganti dengan URL Web App kamu
 
 // Fungsi untuk kirim data ke Google Sheets
 function syncToGoogleSheets(data) {
